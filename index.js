0263696e6a3cec032f0488811be5d8d4a8963f27
Kristina Bezouglova <bezouglovak@gmail.com>
2017-10-04 19:56:31 -0700

    Add API route

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+app.get('/api', function(req, res) {
+  console.log("I hope this works");
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
