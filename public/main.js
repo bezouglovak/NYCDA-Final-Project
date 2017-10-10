61dcf7d433bdddfcfec57a16810ea5ebe2cab8e3
Kristina Bezouglova <bezouglovak@gmail.com>
2017-10-09 18:31:06 -0700

    Added rough html and css styling.

---
+++ public/main.js
@@ -0,0 +1,15 @@
+// $(".dropdown-content .dropdown-btn").hover(function () {
+// 	$(this).children(".dropdown").stop().delay(200).animate({height: "toggle", opacity: "toggle"}, 200);
+// });
+
+// $("dropdown-btn").on("hover", function(h) {
+//   h.preventDefault();
+//
+//   if($(this).hasClass("open")) {
+//     $(this).removeClass("open");
+//     $(this).children("ul").slideUp("fast");
+//   } else {
+//     $(this).addClass("open");
+//     $(this).children("ul").slideDown("fast");
+//   }
+// });
